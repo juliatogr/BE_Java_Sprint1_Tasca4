9ac7915076cd4360a2c36729af65582a77aa502f
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-04 12:01:15 +0200

    n1e2 creation

--- src/n1exercici2/CalculoDni.java
+++ src/n1exercici2/CalculoDni.java
@@ -1,5 +1,13 @@
 package n1exercici2;
 
 public class CalculoDni {
+	
 
+	public char computeLetter(int numDni) {
+		char letters[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};	
+		
+		return letters[numDni%23];
+	}
+	
+	
 }
--- src/n1exercici2/CalculoDniTest.java
+++ src/n1exercici2/CalculoDniTest.java
@@ -2,13 +2,34 @@ package n1exercici2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 class CalculoDniTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	CalculoDni calcDni = new CalculoDni();
+	
+	static Stream<Arguments> arguments = Stream.of(
+	  Arguments.of(47331864,'A'),
+	  Arguments.of(30194029, 'C'),
+	  Arguments.of(48205738, 'S'),
+	  Arguments.of(43859275, 'T'),
+	  Arguments.of(42947593, 'S'),
+	  Arguments.of(59902758, 'V'),
+	  Arguments.of(39574938, 'B'),
+	  Arguments.of(37059475, 'N'),
+	  Arguments.of(41048924, 'L'),
+	  Arguments.of(38049359, 'E')
+
+	);
+	
+	@ParameterizedTest
+	@VariableSource("arguments")
+	void computeLetterTest(int numDni, char expectedLetter) {
+		char actualLetter = calcDni.computeLetter(numDni);
+		assertEquals(expectedLetter, actualLetter);
 	}
 
 }
