19ecdf0f83cebc4a3c32df17114dda6c2174a460
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-07-04 10:11:30 +0200

    refactor names

---
+++ src/n1exercici2/CalculoDniTest.java
@@ -0,0 +1,14 @@
+package n1exercici2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculoDniTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
